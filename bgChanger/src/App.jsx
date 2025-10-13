a3346f12bd4a42891dd9daf4fafe015b0a8e58f9
Aditya Shendkar <aditya.shendkar03@gmail.com>
2025-10-13 15:09:06 +0530

    implement color selection feature with buttons to change background color

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,8 +1,53 @@
+import { useState } from "react";
+
 function App() {
+  const [color, setColor] = useState("black");
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div
+      className="w-full h-screen duration-300"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center top-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl ">
+          <button
+            onClick={() => setColor("Magenta")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "Magenta" }}
+          >
+            Magenta
+          </button>
+          <button
+            onClick={() => setColor("Chartreuse")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "Chartreuse" }}
+          >
+            Chartreuse
+          </button>
+          <button
+            onClick={() => setColor("Turquoise")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "Turquoise" }}
+          >
+            Turquoise
+          </button>
+          <button
+            onClick={() => setColor("purple")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "purple" }}
+          >
+            purple
+          </button>
+          <button
+            onClick={() => setColor("Aqua")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "Aqua" }}
+          >
+            Aqua
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
