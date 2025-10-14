d0d1ec1c33968f213aa71dca03a988c92c792f9a
Aditya Shendkar <aditya.shendkar03@gmail.com>
2025-10-14 12:49:19 +0530

    set up initial state for password generation

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 function App() {
+    const [length, setlength] = useState(10)
+    const [number, setnumber] = useState(false)
+    const [char, setchar] = useState(false)
+    const [password, setpassword] = useState("")
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold text-center ">Password Generator</h1>
     </>
   );
 }
