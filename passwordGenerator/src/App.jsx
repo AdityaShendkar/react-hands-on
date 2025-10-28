203164617e493a1682a66dc2a4ddccf1d70802ce
Aditya Shendkar <aditya.shendkar03@gmail.com>
2025-10-28 18:54:15 +0530

    refactor state variable naming and improve password generation logic

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState, useCallback } from "react";
 
 function App() {
-  const [length, setlength] = useState(10);
-  const [number, setnumber] = useState(false);
-  const [char, setchar] = useState(false);
-  const [password, setpassword] = useState("Random");
+  const [length, setLength] = useState(10);
+  const [number, setNumber] = useState(false);
+  const [char, setChar] = useState(false);
+  const [password, setPassword] = useState("Random");
 
   const passwordGenerator = useCallback(() => {
     let pass = "";
@@ -12,17 +12,19 @@ function App() {
     if (number) str += "0123456789";
     if (char) str += "!@#$%^&*-_+=[]{}~`";
 
-    for (let i = 1; i <= array.length; i++) {
+    for (let i = 1; i <=length; i++) {
       let char = Math.floor(Math.random() * str.length + 1);
       pass += str.charAt(char);
     }
 
-    setpassword(pass);
-  }, [length, number, char, setpassword]);
+    setPassword(pass);
+  }, [length, number, char, setPassword]);
 
+
+  
   return (
     <>
-      <div className="w-full max-w-md mx-auto shadow-lg px-5 py-3 bg-gray-800 text-violet-500 ">
+      <div className="w-full max-w-md mx-auto shadow-lg px-6 py-3 bg-gray-800 text-violet-500 ">
         <h1 className="text-3xl font-bold text-center py-2 text-white ">
           Password Generator
         </h1>
@@ -38,18 +40,43 @@ function App() {
           />
           <button className="bg-slate-200 py-2 px-3 text-lg ">Copy</button>
         </div>
-        <div className="flex text-sm gap-x-2 py-5">
+        <div className="flex text-sm justify-evenly gap-x-2 py-5">
           <div className="flex items-center gap-x-1">
-            <input type="range" 
-            min={6}
-            max={32}
-            value={length}
-            className="cursor-pointer "
-            onChange={() => {}}
+            <input
+              type="range"
+              min={6}
+              max={32}
+              value={length}
+              className="cursor-pointer "
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
             />
-            <label htmlFor="">Length:{length} </label>
+            <label htmlFor="">Length: {length} </label>
           </div>
-         
+          <div className="flex items-center gap-1 ">
+            <input
+              type="checkbox"
+              name=""
+              id="numberInput"
+              defaultChecked={number}
+              onChange={() => {
+                setNumber((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Number </label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={char}
+              id="characterInput"
+              onChange={() => {
+                setChar((prev) => !prev);
+              }}
+            />
+            <label htmlFor="characterInput">Characters</label>
+          </div>
         </div>
       </div>
     </>
